e7e3fe41be5a8274c68a86542b91fa47ea35bdd0
piou <piou@a809.local>
2017-08-30 00:14:55 +0900

    Initial commit

---
+++ copy-workspaces.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+BASEDIR=$(dirname "$0")
+cp $BASEDIR/default "$HOME/Library/Preferences/Adobe Illustrator 21 Settings/ja_JP/ワークスペース/default"
+cp $BASEDIR/workspace.xml "$HOME/Library/Preferences/Adobe InDesign/Version 12.0-J/ja_JP/Workspaces/workspace.xml"
+cp $BASEDIR/workspace.psw "$HOME/Library/Preferences/Adobe Photoshop CC 2017 Settings/WorkSpaces/workspace.psw"
